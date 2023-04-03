5bb56b2a55eb437e273d3e16178f3f67ddf75692
Tom323-323 <tomas0dft@gmail.com>
2023-04-03 20:13:59 +0300

    03/04 - Add search fun.

--- app/src/main/java/com/example/lessonretrofit/MainActivity.kt
+++ app/src/main/java/com/example/lessonretrofit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lessonretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lessonretrofit.adapter.RvAdapter
 import com.example.lessonretrofit.databinding.ActivityMainBinding
@@ -41,13 +42,27 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create()).build()
         val mainApi = retrofit.create(MainApi::class.java)
 
-            CoroutineScope(Dispatchers.IO).launch {
-                val list = mainApi.getAllProducts()
+
+        binding.searchView.setOnQueryTextListener(object :OnQueryTextListener{
+            override fun onQueryTextSubmit(text: String?): Boolean {
+
+                return true
+            }
+
+            override fun onQueryTextChange(text: String?): Boolean {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val list = text?.let { mainApi.getProductsByName(it) }
                     runOnUiThread {
                         binding.apply {
-                            adapter.submitList(list.products)
+                            adapter.submitList(list?.products)
                         }
                     }
+                }
+                return true
             }
+
+        })
+
+
     }
 }
--- app/src/main/java/com/example/lessonretrofit/retrofit/MainApi.kt
+++ app/src/main/java/com/example/lessonretrofit/retrofit/MainApi.kt
@@ -4,6 +4,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MainApi {
     @GET("products/{id}")
@@ -15,5 +16,7 @@ interface MainApi {
     @GET("products")
     suspend fun getAllProducts():Products
 
+    @GET("products/search")
+    suspend fun getProductsByName(@Query ("q") name:String):Products
 
 }
